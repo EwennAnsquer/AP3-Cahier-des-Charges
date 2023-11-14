48005f94e9cb07b8cbe6da229a3b66984d0922b2
EwennAnsquer <ewenn.ansq@gmail.com>
2023-11-14 15:19:25 +0100

    possibilité de supprimé et modifier les centres de colis

--- src/Controller/CentreRelaisColisController.php
+++ src/Controller/CentreRelaisColisController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\CentreRelaisColis;
 use App\Repository\CentreRelaisColisRepository;
+use App\Form\CentreRelaisColisModifyType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class CentreRelaisColisController extends AbstractController
 {
@@ -25,5 +28,49 @@ class CentreRelaisColisController extends AbstractController
             'user' => $user,
             'centreRelaisColis' => $allCentreRelaisColis,
         ]);
+    }
+
+    #[Route('/CentreRelaisColis/modify/{id}', name: 'app_centre_relais_colis_modify')]
+    public function modify(CentreRelaisColis $centreRelaisColis, Request $request, EntityManagerInterface $manager): Response
+    {
+        if ($this->getUser()==false) {
+            return $this->redirectToRoute('app_login');
+        }else{
+            $user = $this->getUser();
+        }
+
+        $centreRelaisColisModify = $centreRelaisColis;
+        $form = $this->createForm(CentreRelaisColisModifyType::class, $centreRelaisColisModify);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($centreRelaisColisModify);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_centre_relais_colis');
+        }
+
+        return $this->render('centre_relais_colis/modify.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/CentreRelaisColis/delete/{id}', name: 'app_centre_relais_colis_delete')]
+    public function delete(CentreRelaisColis $entite,Request $request, EntityManagerInterface $manager): Response
+    {
+
+        if (!$entite) {
+            throw $this->createNotFoundException('Entité non trouvée');
+        }
+
+        $manager->remove($entite);
+        $manager->flush();
+
+        $this->addFlash('success', 'Entité supprimée avec succès.');
+
+        // Redirigez vers la page d'où provient la requête
+        return $this->redirect($request->headers->get('referer'));
     }
 }

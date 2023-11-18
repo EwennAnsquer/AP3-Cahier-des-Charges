76ccc676fbc18eedb23685be57db93b9835a1443
EwennAnsquer <ewenn.ansq@gmail.com>
2023-11-18 17:20:42 +0100

    corection du responsive de la navbar

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -5,19 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\CompteUtilisateur;
+use App\Repository\CompteUtilisateurRepository;
 
 class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
-    public function index(): Response
+    #[Route('/Client', name: 'app_client')]
+    public function index(CompteUtilisateurRepository $compteUtilisateurRepository): Response
     {
         if ($this->getUser()==false) {
             return $this->redirectToRoute('app_login');
         }else{
             $user = $this->getUser();
         }
+
+        $allCompteUtilisateur = $compteUtilisateurRepository->findAll();
+
         return $this->render('client/index.html.twig', [
             'user' => $user,
+            'compteUtilisateur' => $allCompteUtilisateur,
         ]);
     }
 }

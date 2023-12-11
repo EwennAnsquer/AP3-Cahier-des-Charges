21d9af3a0794c215e98128e8a1d21d0259603072
EwennAnsquer <ewenn.ansq@gmail.com>
2023-12-11 17:16:27 +0100

    Affichage, modification et suppression des commandes

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -10,19 +10,24 @@ use App\Repository\CompteUtilisateurRepository;
 use App\Form\CommandeAddType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommandeRepository;
 
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: 'app_commande')]
-    public function index(CompteUtilisateurRepository $c): Response
+    public function index(CompteUtilisateurRepository $c, CommandeRepository $commandeRepository): Response
     {
         if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
             return $this->redirectToRoute('app_login');
         }else{
             $user = $this->getUser();
         }
+
+        $allCommande = $commandeRepository->findAll();
+
         return $this->render('commande/index.html.twig', [
             'user' => $user,
+            'commandes'=> $allCommande
         ]);
     }
 
@@ -56,5 +61,53 @@ class CommandeController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/commande/modify/{id}', name: 'app_commande_modify')]
+    public function modify(Commande $commande, Request $request, CompteUtilisateurRepository $c, EntityManagerInterface $e): Response
+    {
+        if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
+            return $this->redirectToRoute('app_login');
+        }else{
+            $user = $this->getUser();
+        }
+
+        $form = $this->createForm(CommandeAddType::class, $commande);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $e->persist($commande);
+            $e->flush();
+
+            $this->addFlash('success', 'La ligne a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_commande');
+        }
+
+        return $this->render('commande/modify.html.twig',[
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/commande/delete/{id}', name: 'app_commande_delete')]
+    public function delete(Commande $entite,Request $request, EntityManagerInterface $manager, CompteUtilisateurRepository $c): Response
+    {
+        if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if (!$entite) {
+            throw $this->createNotFoundException('Entité non trouvée');
+        }
+
+        $manager->remove($entite);
+        $manager->flush();
+
+        $this->addFlash('success', 'Entité supprimée avec succès.');
+
+        // Redirigez vers la page d'où provient la requête
+        return $this->redirect($request->headers->get('referer'));
     }
 }

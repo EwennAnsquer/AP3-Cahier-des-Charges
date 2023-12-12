f021496826616e1a374434af993edc0b102f6a4d
EwennAnsquer <ewenn.ansq@gmail.com>
2023-12-12 15:47:55 +0100

    possibilité de voir et de supprimer les colis

--- src/Controller/CentreRelaisColisController.php
+++ src/Controller/CentreRelaisColisController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\CentreRelaisColis;
+use App\Entity\Commande;
 use App\Form\CentreRelaisColisAddType;
 use App\Repository\CentreRelaisColisRepository;
 use App\Form\CentreRelaisColisModifyType;
--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Colis;
 use App\Entity\Commande;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -105,6 +106,74 @@ class CommandeController extends AbstractController
 
     #[Route('/commande/delete/{id}', name: 'app_commande_delete')]
     public function delete(Commande $entite,Request $request, EntityManagerInterface $manager, CompteUtilisateurRepository $c): Response
+    {
+        if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if (!$entite) {
+            throw $this->createNotFoundException('Entité non trouvée');
+        }
+
+        $manager->remove($entite);
+        $manager->flush();
+
+        $this->addFlash('success', 'Entité supprimée avec succès.');
+
+        // Redirigez vers la page d'où provient la requête
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    #[Route('/commande/colis/{id}', name: 'app_commande_colis')]
+    public function colis(Commande $commande, CompteUtilisateurRepository $c): Response
+    {
+        if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $allColis = $commande->getLesColis();
+
+        return $this->render('commande/colis.html.twig', [
+            'colis' => $allColis
+        ]);
+    }
+
+    #[Route('/commande/colis/modify/{id}', name: 'app_commande_colis_modify')]
+    public function modifyColis(Commande $commande, Request $request, CompteUtilisateurRepository $c, EntityManagerInterface $e): Response
+    {
+        if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
+            return $this->redirectToRoute('app_login');
+        }else{
+            $user = $this->getUser();
+        }
+
+        $form = $this->createForm(CommandeAddType::class, $commande);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->get('NumeroTelephone')->getData();
+
+            if (preg_match('/^\d{10}$/', $data)) { //vérifie si NumeroTelephone contient 10 chiffres
+                $e->persist($commande);
+                $e->flush();
+    
+                $this->addFlash('success', 'La ligne a été modifié avec succès.');
+    
+                return $this->redirectToRoute('app_commande');
+            }else {
+                $this->addFlash('error', 'Le numéro de téléphone doit contenir exactement 10 chiffres.');
+            }
+        }
+
+        return $this->render('commande/modify.html.twig',[
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/commande/colis/delete/{id}', name: 'app_commande_colis_delete')]
+    public function deleteColis(Colis $entite,Request $request, EntityManagerInterface $manager, CompteUtilisateurRepository $c): Response
     {
         if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
             return $this->redirectToRoute('app_login');

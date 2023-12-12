26f5d44e6cf109f1b0ce5b2f07bb5d08baf1bdb3
EwennAnsquer <ewenn.ansq@gmail.com>
2023-12-12 19:23:19 +0100

    Permet de voir les notifications de l'application

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CompteUtilisateurRepository;
+use App\Repository\NotificationRepository;
 
 class NotificationController extends AbstractController
 {
     #[Route('/notification', name: 'app_notification')]
-    public function index(CompteUtilisateurRepository $c): Response
+    public function index(CompteUtilisateurRepository $c, NotificationRepository $n): Response
     {
         if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
             return $this->redirectToRoute('app_login');
@@ -22,8 +23,11 @@ class NotificationController extends AbstractController
             return $this->redirectToRoute('app_rapports');
         }
 
+        $allNotification = $n->findAll();
+
         return $this->render('notification/index.html.twig', [
             'user' => $user,
+            "notifications" => $allNotification
         ]);
     }
 }

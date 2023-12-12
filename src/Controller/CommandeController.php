beffd91d6ad81600dcdd6792e7f1980f6a34f068
EwennAnsquer <ewenn.ansq@gmail.com>
2023-12-12 16:11:13 +0100

    possibilité de modifier les colis

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Colis;
 use App\Entity\Commande;
+use App\Form\ColisModifyType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -139,36 +140,30 @@ class CommandeController extends AbstractController
     }
 
     #[Route('/commande/colis/modify/{id}', name: 'app_commande_colis_modify')]
-    public function modifyColis(Commande $commande, Request $request, CompteUtilisateurRepository $c, EntityManagerInterface $e): Response
+    public function modifyColis(Colis $colis, Request $request, CompteUtilisateurRepository $c, EntityManagerInterface $e): Response
     {
         if ($this->getUser()==false or $c->find($this->getUser())->isIsRegister()==false) {
             return $this->redirectToRoute('app_login');
-        }else{
-            $user = $this->getUser();
         }
 
-        $form = $this->createForm(CommandeAddType::class, $commande);
+        $form = $this->createForm(ColisModifyType::class, $colis);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->get('NumeroTelephone')->getData();
-
-            if (preg_match('/^\d{10}$/', $data)) { //vérifie si NumeroTelephone contient 10 chiffres
-                $e->persist($commande);
+                $e->persist($colis);
                 $e->flush();
     
                 $this->addFlash('success', 'La ligne a été modifié avec succès.');
     
-                return $this->redirectToRoute('app_commande');
-            }else {
-                $this->addFlash('error', 'Le numéro de téléphone doit contenir exactement 10 chiffres.');
-            }
+                return $this->redirectToRoute('app_commande_colis',[
+                    'id' => $colis->getId()
+                ]);
         }
 
-        return $this->render('commande/modify.html.twig',[
-            'user' => $user,
+        return $this->render('commande/modifyColis.html.twig',[
             'form' => $form->createView(),
+            'id' => $colis->getId()
         ]);
     }
 
